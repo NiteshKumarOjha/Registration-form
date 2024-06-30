384b1c8f9c0ad5ab313bb5b49a1f2b334501f117
Nitesh Kumar Ojha <nitesh303607@gmail.com>
2024-07-01 02:51:44 +0530

    create Header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <div className="bg-black bg-opacity-90">
+      <div className="flex justify-between items-center max-w-6xl mx-auto h-10">
+        <div className="bg-yellow-200 flex items-center h-full p-3">
+          <Link to="/">
+            <h1 className="font-bold">Registration Form</h1>
+          </Link>
+        </div>
+        <div>
+          <ul className="flex gap-6 text-white">
+            <Link to="/home">
+              <li>Home</li>
+            </Link>
+            <Link to="/about">
+              <li>About</li>
+            </Link>
+            <Link to="/sign-in">
+              <li>Sign In</li>
+            </Link>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
