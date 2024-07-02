de3a655ea8550b2b56798fae085d0a98b0471684
Nitesh Kumar Ojha <nitesh303607@gmail.com>
2024-07-02 07:07:15 +0530

    create update user API

--- api/index.js
+++ api/index.js
@@ -1,21 +1,29 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-dotenv.config();
-
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
-  .then(() => console.log("connected to mongoDB"))
-  .catch((err) => console.log(err));
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(3000, () => console.log("Server listening on port 3000"));
+app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
@@ -25,7 +33,7 @@ app.use((err, req, res, next) => {
   const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
-    error: message,
-    statusCode: statusCode,
+    message,
+    statusCode,
   });
 });
